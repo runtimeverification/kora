dd4eaa37f5f55dcb444142870ef629d685e81987
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2025-09-22 11:29:43 -0500

    Move Arbitrary impls to util mod

--- crates/lib/fuzz/fuzz_targets/fuzz_target_1.rs
+++ crates/lib/fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,51 +1,17 @@
 #![no_main]
 
+mod util;
 use arbitrary::{Arbitrary, Unstructured};
 use kora_lib::{
-    config,
-    config::EnabledMethods,
-    fee::price::{PriceConfig, PriceModel},
     signer::{KoraSigner, SolanaMemorySigner},
     state::update_config,
-    tests::{
-        config_mock::ConfigMockBuilder, rpc_mock::RpcMockBuilder,
-        transaction_mock::create_mock_transaction,
-    },
+    tests::{rpc_mock::RpcMockBuilder, transaction_mock::create_mock_transaction},
     transaction::{VersionedTransactionOps, VersionedTransactionResolved},
-    Config,
 };
 use libfuzzer_sys::fuzz_target;
 use solana_sdk::signature::Keypair;
 use std::sync::Arc;
-
-struct FuzzConfig {
-    kora_config: Config,
-}
-
-impl<'a> Arbitrary<'a> for FuzzConfig {
-    fn arbitrary(u: &mut Unstructured<'a>) -> libfuzzer_sys::arbitrary::Result<Self> {
-        let mut config = ConfigMockBuilder::new().build();
-
-        // Validation config
-        let validation = &mut config.validation;
-        validation.max_allowed_lamports = u64::arbitrary(u)?;
-        validation.max_signatures = u64::arbitrary(u)?;
-
-        validation.fee_payer_policy = config::FeePayerPolicy::arbitrary(u)?;
-
-        let n: u64 = u.arbitrary()?;
-        let margin = (n as f64) / (u64::MAX as f64);
-        validation.price = PriceConfig { model: PriceModel::Margin { margin: margin } };
-
-        validation.token_2022 = config::Token2022Config::arbitrary(u)?;
-
-        // Kora config
-        let kora = &mut config.kora;
-        kora.enabled_methods = EnabledMethods::arbitrary(u)?;
-
-        Ok(Self { kora_config: config })
-    }
-}
+use util::FuzzConfig;
 
 fuzz_target!(|data: &[u8]| {
     let mut u = Unstructured::new(data);
