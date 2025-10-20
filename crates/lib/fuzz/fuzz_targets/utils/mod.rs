ec70a1bfffaa10ab69cdec881bfdfd1b02b438c3
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2025-10-20 15:10:01 -0500

    Remove arbitrary transaction

--- crates/lib/fuzz/fuzz_targets/utils/mod.rs
+++ crates/lib/fuzz/fuzz_targets/utils/mod.rs
@@ -3,8 +3,6 @@ pub mod config;
 pub mod litesvm;
 pub mod spl_token;
 pub mod spl_token_2022;
-pub mod transaction;
 
 pub use config::FuzzConfig;
 pub use litesvm::LiteSVMSender;
-pub use transaction::{FuzzInstruction, FuzzTransaction};
