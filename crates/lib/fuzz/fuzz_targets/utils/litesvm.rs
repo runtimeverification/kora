74c203a2dfccc273f4721d77b367c3737a4e97a5
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2025-10-23 21:43:30 -0500

    Implement simulateTransaction for LiteSVM RPC

--- crates/lib/fuzz/fuzz_targets/utils/litesvm.rs
+++ crates/lib/fuzz/fuzz_targets/utils/litesvm.rs
@@ -1,14 +1,28 @@
 use async_trait::async_trait;
-use litesvm::LiteSVM;
+use base64::{engine::general_purpose::STANDARD, Engine};
+use kora_lib::transaction::TransactionUtil;
+use litesvm::{
+    types::{FailedTransactionMetadata, SimulatedTransactionInfo, TransactionMetadata},
+    LiteSVM,
+};
 use serde_json::json;
-use solana_account_decoder::{encode_ui_account, UiAccountEncoding};
+use solana_account_decoder::{encode_ui_account, UiAccount, UiAccountData, UiAccountEncoding};
 use solana_client::{
     client_error::Result,
+    rpc_config::RpcSimulateTransactionConfig,
     rpc_request::RpcRequest,
-    rpc_response::{Response, RpcResponseContext},
+    rpc_response::{Response, RpcResponseContext, RpcSimulateTransactionResult},
     rpc_sender::{RpcSender, RpcTransportStats},
 };
-use solana_sdk::pubkey::Pubkey;
+use solana_sdk::{
+    account::{Account, AccountSharedData},
+    pubkey::Pubkey,
+    transaction::{TransactionError, VersionedTransaction},
+    transaction_context::TransactionReturnData,
+};
+use solana_transaction_status_client_types::{
+    InnerInstruction, InnerInstructions, UiInnerInstructions,
+};
 
 pub struct LiteSVMSender(pub LiteSVM);
 
@@ -44,7 +58,48 @@ impl RpcSender for LiteSVMSender {
             "getLatestBlockhash" => todo!("latest_blockhash"),
             "getTransaction" => todo!("get_transaction"),
             "sendTransaction" => todo!("send_transaction"),
-            "simulateTransaction" => todo!("simulate_transaction"),
+            "simulateTransaction" => {
+                let config = serde_json::from_value::<(String, Option<RpcSimulateTransactionConfig>)>(
+                    params,
+                );
+                if let Ok((tx, _config)) = config {
+                    let versioned = TransactionUtil::decode_b64_transaction(&tx).unwrap();
+                    let simulated_result = self.0.simulate_transaction(versioned);
+
+                    let (metadata, res) = match simulated_result {
+                        Ok(SimulatedTransactionInfo { meta, post_accounts }) => {
+                            (meta, Ok(post_accounts))
+                        }
+                        Err(FailedTransactionMetadata { err, meta }) => (meta, Err(err)),
+                    };
+
+                    let TransactionMetadata {
+                        signature: _,
+                        logs,
+                        inner_instructions,
+                        compute_units_consumed,
+                        return_data,
+                    } = metadata;
+
+                    let ui_instructions =
+                        inner_instructions_to_ui_instructions(&inner_instructions);
+                    let accounts = res.as_ref().ok().map(post_accounts_to_ui_accounts);
+
+                    serde_json::to_value(RpcSimulateTransactionResult {
+                        err: res.err(),
+                        logs: Some(logs),
+                        accounts: accounts,
+                        units_consumed: Some(compute_units_consumed),
+                        loaded_accounts_data_size: None, // ?
+                        return_data: Some(return_data.into()),
+                        inner_instructions: Some(ui_instructions),
+                        replacement_blockhash: None,
+                    })
+                    .unwrap()
+                } else {
+                    serde_json::Value::Null
+                }
+            }
             "getFeeForMessage" => json!(5000 as u64),
             _ => todo!("unsupported method, {}, {}", method, param_str),
         };
@@ -61,4 +116,50 @@ impl RpcSender for LiteSVMSender {
     fn url(&self) -> String {
         String::default()
     }
+}
+
+fn post_accounts_to_ui_accounts(
+    metadata: &Vec<(Pubkey, AccountSharedData)>,
+) -> Vec<Option<UiAccount>> {
+    metadata
+        .iter()
+        .map(|(_, account_shared_data)| {
+            let account: Account = account_shared_data.clone().into();
+            let data =
+                UiAccountData::Binary(STANDARD.encode(account.data), UiAccountEncoding::Base64);
+            Some(UiAccount {
+                lamports: account.lamports,
+                data: data,
+                owner: account.owner.to_string(),
+                executable: account.executable,
+                rent_epoch: account.rent_epoch,
+                space: None,
+            })
+        })
+        .collect()
+}
+
+fn inner_instructions_to_ui_instructions(
+    inner_instructions: &Vec<Vec<solana_message::inner_instruction::InnerInstruction>>,
+) -> Vec<UiInnerInstructions> {
+    inner_instructions
+        .iter()
+        .map(|ixs| {
+            ixs.iter()
+                .map(
+                    // Convert LiteSVM's solana_message InnerInstruction to
+                    // the solana_rpc_types InnerInstruction
+                    |solana_message::inner_instruction::InnerInstruction {
+                         instruction,
+                         stack_height,
+                     }| InnerInstruction {
+                        instruction: instruction.clone(),
+                        stack_height: Some(*stack_height as u32),
+                    },
+                )
+                .collect::<Vec<InnerInstruction>>()
+        })
+        .enumerate()
+        .map(|(i, x)| InnerInstructions { index: i as u8, instructions: x.clone() }.into())
+        .collect()
 }
